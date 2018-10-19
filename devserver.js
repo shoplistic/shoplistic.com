bfcb75e617ff4225df87efd9dcf581df6f8c5f74
Edwin Svensson <git@olback.net>
2018-10-19 22:25:26 +0200

    Initial commit

---
+++ devserver.js
@@ -0,0 +1,27 @@
+const concurrently = require('concurrently');
+const env = require('process').env;
+
+env.NODE_ENV = 'dev';
+env.PORT = 5000;
+env.PASSWORD = 'Cz597QJ30rm63yUor09Z';
+
+concurrently([{
+        command: 'npm:ts:watch',
+        name: 'Typescript',
+        prefixColor: ['white', 'bgBlue', 'bold']
+    },
+    {
+        command: 'npm:scss:watch',
+        name: 'Scss',
+        prefixColor: ['white', 'bgMagenta', 'bold']
+    },
+    {
+        command: 'npm:serve',
+        name: 'Server',
+        prefixColor: ['white', 'bgGreen', 'bold']
+    }
+], {
+    prefix: 'name',
+    killOthers: ['failure', 'success'],
+    restartTries: 3,
+});
